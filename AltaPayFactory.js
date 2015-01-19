60238a868ee494e959bcc00be2dc3249c6fedf76
patrick <pt@altapay.com>
2015-01-19 08:46:35 +0100

    What I have so far... the rest will have to be completed as part of the user story

--- AltaPayFactory.js
+++ AltaPayFactory.js
@@ -1,16 +1,59 @@
 
 function AltaPayFactory() {
-
 }
 
-AltaPayFactory.prototype.getMerchantApi = function() {
-    return new MerchantApi(this, this.getLogger(), this.getHttp());
+/**
+ * @returns {MerchantApi}
+ */
+AltaPayFactory.prototype.getMerchantApi = function(username, password, url) {
+    return new MerchantApi(
+          username
+        , password
+        , url
+        , this
+        , this.getLogger()
+        , this.getHttp()
+    );
 };
 
+/**
+ * @returns {ProcessorApi}
+ */
+AltaPayFactory.prototype.getProcessorApi = function(username, password, url) {
+    return new ProcessorApi(
+          username
+        , password
+        , url
+        , this
+        , this.getLogger()
+        , this.getHttp()
+    );
+};
+
+/**
+ * @returns {Logger}
+ */
 AltaPayFactory.prototype.getLogger = function() {
     return new Logger();
 };
 
+/**
+ * @returns {Http}
+ */
 AltaPayFactory.prototype.getHttp = function() {
     return new Http();
+};
+
+/**
+ * @returns {PaymentRequest}
+ */
+AltaPayFactory.prototype.getPaymentRequest = function() {
+    return new PaymentRequest(this.getPaymentRequestConfig());
+};
+
+/**
+ * @returns {PaymentRequestConfig}
+ */
+AltaPayFactory.prototype.getPaymentRequestConfig = function() {
+    return new PaymentRequestConfig();
 };
