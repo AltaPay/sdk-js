6d8a889dcd29086deba623b691471539eb372bdf
anton <al@pensio.dk>
2015-02-20 13:05:15 +0100

    [44] Refactored integration tests to use a test class as well, and prepared for writing junit output

---
+++ tests/basetest.js
@@ -0,0 +1,118 @@
+
+
+load('tests/rhino_stuff/rhino_listFiles.js');
+load('tests/rhino_stuff/rhino_load_sdk.js');
+load('tests/jshamcrest.js');
+load('tests/jsmockito-1.0.4.js');
+
+var Assert = {
+	equals : function(expected, actual, message)
+	{
+		if(message == null)
+		{
+			message = "'"+expected+"' was unexpectedly different from: '"+actual+"'";
+		}
+		if(expected != actual)
+		{
+			throw message;
+		}
+	}
+};
+
+function Test() {
+
+	this.testFile = null;
+	this.testMethod = null;
+
+
+	var params = java.lang.System.getProperty("sun.java.command").split(" ");
+
+	if(params[2] && (params[2].trim().length() > 0))
+	{
+		this.testFile = baseName(params[2]);
+	}
+
+	if(params[3] && params[3].trim().length() > 0)
+	{
+		this.testMethod = params[3];
+	}
+
+}
+
+Test.prototype.baseName = function (str)
+{
+	var base = new String(str).substring(str.lastIndexOf('/') + 1);
+	if(base.lastIndexOf(".") != -1)
+		base = base.substring(0, base.lastIndexOf("."));
+	return base;
+};
+
+Test.prototype.run = function(fromDir)
+{
+	var testFiles = listFiles(fromDir);
+	console.log("Running tests:");
+	var errors = [];
+	for (var i=0; i<testFiles.length; i++) {
+		load(testFiles[i].getCanonicalFile());
+
+		var testObjectName = testFiles[i].getName().replace('.js','');
+
+		if(this.testFile != null && this.testFile != testObjectName)
+		{
+			continue;
+		}
+
+		console.log(testObjectName);
+
+		eval.call(null,"var testObject = "+testObjectName);
+		for(var test in testObject)
+		{
+			if(test == 'setup')
+			{
+				continue;
+			}
+			if(this.testMethod != null && this.testMethod != test)
+			{
+				continue;
+			}
+			try
+			{
+				if(testObject['setup'])
+				{
+					testObject.setup();
+				}
+				testObject[test]();
+				console.logChar('.');
+			}
+			catch(e)
+			{
+				errors.push(testObjectName+"."+test+": "+e);
+				console.logChar('e');
+			}
+		}
+
+		console.log("");
+
+		if(errors.length > 0)
+		{
+			console.log("");
+			console.log("Errors:");
+			for(var i in errors)
+			{
+				console.log(errors[i]);
+			}
+		}
+		console.log("");
+
+
+	}
+
+};
+
+
+
+
+
+
+
+
--- tests/integration_suite.js
+++ tests/integration_suite.js
@@ -1,10 +1,5 @@
-#!/usr/bin/env node
 
-load('tests/rhino_stuff/rhino_listFiles.js');
-load('tests/rhino_stuff/rhino_load_sdk.js');
+load('tests/basetest.js');
 
-var testFiles = listFiles('tests/integration/');
-
-for (var i=0; i<testFiles.length; i++) {
-    load(testFiles[i].getCanonicalFile());
-}
+var test = new Test();
+test.run('tests/integration/');
