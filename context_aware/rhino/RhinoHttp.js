0a1130807fb6ce5ab4f021f57378716b32b3a5ba
rolandas <rb@altapay.com>
2015-02-20 19:38:51 +0100

    [58] Add method for login and for inputStream to String conversion;

--- context_aware/rhino/RhinoHttp.js
+++ context_aware/rhino/RhinoHttp.js
@@ -3,7 +3,7 @@
  * @constructor
  */
 function RhinoHttp() {
-
+	this.xml = new RhinoXml();
 }
 
 /**
@@ -26,3 +26,39 @@ RhinoHttp.prototype.get = function(url, parameters, headers) {
 RhinoHttp.prototype.post = function(url, parameters, headers) {
 
 };
+
+/**
+ * Do a login request
+ * @param url
+ * @param username
+ * @param password
+ * @returns {boolean}
+ */
+RhinoHttp.prototype.login = function(url, username, password) {
+	var charset = "UTF-8";
+	var authEncBytes = javax.xml.bind.DatatypeConverter.printBase64Binary(java.lang.String(username + ":" + password).getBytes());
+
+	var connection = java.net.URL(url).openConnection();
+	connection.setRequestProperty("Authorization", "Basic " + java.lang.String(authEncBytes));
+	connection.setRequestProperty("Accept-Charset", charset);
+
+	var xml = this.xml.deserialize(inputStreamToString(connection.getInputStream()));
+	return 'OK' === xml.Body.Result;
+}
+
+/**
+ * Read response input stream into a string
+ * @param is
+ * @returns {string}
+ */
+function inputStreamToString(is){
+	var ch;
+	var sb = java.lang.StringBuilder();
+	var reader = java.io.InputStreamReader(is);
+
+	while((ch = reader.read())!= -1)
+	{
+		sb.append(java.lang.Character(ch));
+	}
+	return sb.toString();
+}
