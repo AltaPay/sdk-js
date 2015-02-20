00b253b5dfca09656afba6d63a91de3a45a69b84
anton <al@pensio.dk>
2015-02-20 11:17:01 +0100

    [65] Added a totally minor test framework, that just runs methods in an object named the same as the file... :-)

--- context_aware/rhino/rhino_console.js
+++ context_aware/rhino/rhino_console.js
@@ -3,6 +3,10 @@ var console = {
 
     log: function(msg) {
         java.lang.System.out.println(msg);
-    }
+    },
+
+	logChar: function(msg) {
+		java.lang.System.out.print(msg);
+	}
 
 };
--- tests/unit_suite.js
+++ tests/unit_suite.js
@@ -15,11 +15,46 @@ var Assert = {
 			throw message;
 		}
 	}
-
 };
 
-var testFiles = listFiles('tests/unit/');
 
+
+var testFiles = listFiles('tests/unit/');
+console.log("Running tests:");
+var errors = [];
 for (var i=0; i<testFiles.length; i++) {
 	load(testFiles[i].getCanonicalFile());
+
+	var testObjectName = testFiles[i].getName().replace('.js','');
+	console.log(testObjectName);
+
+	eval.call(null,"var testObject = "+testObjectName);
+	for(var test in testObject)
+	{
+		try
+		{
+			testObject[test]();
+			console.logChar('.');
+		}
+		catch(e)
+		{
+			errors.push(testObjectName+"."+test+": "+e);
+			console.logChar('e');
+		}
+	}
+
+	console.log("");
+
+	if(errors.length > 0)
+	{
+		console.log("");
+		console.log("Errors:");
+		for(var i in errors)
+		{
+			console.log(errors[i]);
+		}
+	}
+	console.log("");
+
+
 }
