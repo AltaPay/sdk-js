8adeb9271a2313a0acc989397cc83cebdb5c783a
anton <al@pensio.dk>
2015-02-20 10:55:21 +0100

    [65] Now we can parse an api response somewhat. Also added an Assert.equals() method, which makes testing easier.

--- context_aware/rhino/RhinoXml.js
+++ context_aware/rhino/RhinoXml.js
@@ -14,5 +14,50 @@ function RhinoXml() {
 RhinoXml.prototype.deserialize = function(xmlString) {
 	var builder = javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder()
 
-	builder.parse(new org.xml.sax.InputSource(new java.io.StringReader(xmlString)));
+	var doc = builder.parse(new org.xml.sax.InputSource(new java.io.StringReader(xmlString)));
+
+	var iterator = doc.createNodeIterator(doc.getDocumentElement(), org.w3c.dom.traversal.NodeFilter.SHOW_ELEMENT, null, true);
+
+	return this.traverseXmlDom(doc.getDocumentElement());
 };
+
+
+RhinoXml.prototype.traverseXmlDom = function(element) {
+
+	var childNodes = element.getChildNodes();
+	var result = {};
+//	console.log(element.getTagName()+ "("+childNodes.getLength()+")");
+
+	if(element.getAttributes() != null)
+	{
+		var attributes = element.getAttributes();
+		for(var a = 0;a < attributes.getLength();a++)
+		{
+			var attribute = attributes.item(a);
+			result['@'+attribute.getName()] = attribute.getTextContent();
+		}
+	}
+
+	for(var i = 0;i < childNodes.getLength();i++)
+	{
+		var childNode = childNodes.item(i);
+
+
+
+		if(childNode.getNodeType() == 3)
+		{
+			return childNode.getTextContent();
+		}
+		else if(childNode.getNodeType() == 1)
+		{
+			result[childNode.getTagName()] = this.traverseXmlDom(childNode);
+
+		}
+		else
+		{
+			throw "Unknown node type: "+childNode.getNodeType();
+		}
+
+	}
+	return result;
+}
--- tests/unit_suite.js
+++ tests/unit_suite.js
@@ -3,6 +3,21 @@
 load('tests/rhino_stuff/rhino_listFiles.js');
 load('tests/rhino_stuff/rhino_load_sdk.js');
 
+var Assert = {
+	equals : function(expected, actual, message)
+	{
+		if(message == null)
+		{
+			message = "'"+expected+"' was unexpectedly different from: '"+actual+"'";
+		}
+		if(expected != actual)
+		{
+			throw message;
+		}
+	}
+
+};
+
 var testFiles = listFiles('tests/unit/');
 
 for (var i=0; i<testFiles.length; i++) {
