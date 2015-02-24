e1d17d8359e17ba46c4f161a17597de8af74c18f
anton <al@pensio.dk>
2015-02-24 13:51:41 +0100

    [36] implemented tests for refund in merhant api

---
+++ tests/unit/MerchantApiRefundTests.js
@@ -0,0 +1,66 @@
+
+var factory;
+var logger;
+var http;
+var dateHelper;
+var xml;
+var responseFactory;
+var request;
+
+var api;
+
+var MerchantApiRefundTests = {
+
+	setup :function ()
+	{
+		factory = JsMockito.mock(AltaPayFactory);
+		logger = JsMockito.mock(Logger);
+		http = JsMockito.mock(Http);
+		dateHelper = JsMockito.mock(DateHelper);
+		xml = JsMockito.mock(Xml);
+		request = JsMockito.mock(CaptureRequest);
+		responseFactory = JsMockito.mock(ResponseFactory);
+		api = new MerchantApi('username','password','url',factory, logger, http, dateHelper, xml,responseFactory);
+	},
+
+	callHttpWithCorrectParameters : function()
+	{
+		var theHash = {the:'hash'};
+		JsMockito.when(http).post(JsHamcrest.Matchers.anything(),JsHamcrest.Matchers.anything()).thenReturn("Hat!");
+		JsMockito.when(request).toHash().thenReturn(theHash);
+
+		api.refund(request);
+
+		JsMockito.verify(http).post('url/merchant/API/refundCapturedReservation', theHash);
+	},
+
+	deserializeResponseFromPost : function()
+	{
+		JsMockito.when(http).post(JsHamcrest.Matchers.anything(),JsHamcrest.Matchers.anything()).thenReturn("Hat!");
+
+		api.refund(request);
+
+		JsMockito.verify(xml).deserialize("Hat!");
+	},
+
+	getResponseFromFactory : function()
+	{
+		var response = {the:'response'};
+		JsMockito.when(xml).deserialize(JsHamcrest.Matchers.anything()).thenReturn(response);
+
+		api.refund(request);
+
+		JsMockito.verify(responseFactory).getRefundResponse(response);
+	},
+
+	returnResponseFromFactory : function()
+	{
+		var response = new RefundResponse({});
+		JsMockito.when(responseFactory).getRefundResponse(JsHamcrest.Matchers.anything()).thenReturn(response);
+
+		var actual = api.refund(request);
+
+		Assert.equals(response,actual);
+	}
+
+};
