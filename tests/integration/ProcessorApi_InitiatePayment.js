cb355619de2f5d0574e848c2fc9efb68e2346b7c
anton <al@pensio.dk>
2015-02-25 11:10:53 +0100

    [37] Added support for sale reconciliation identifier in initiate payment

--- tests/integration/ProcessorApi_InitiatePayment.js
+++ tests/integration/ProcessorApi_InitiatePayment.js
@@ -76,6 +76,27 @@ var ProcessorApi_InitiatePayment = {
 		Assert.equals(true, response.success());
 		Assert.equals('hat', response.getPaymentInfo('wah_wah'));
 		Assert.equals('payment info', response.getPaymentInfo('another'));
+	},
+
+	initiatePayment_reconciliationIdentifier : function()
+	{
+		var request = factory.getInitiatePaymentRequest();
+
+		request.amount = 30.30;
+		request.terminal = 'AltaPay Test Terminal';
+		request.currency = 'EUR';
+		request.creditCard.cardnum = '4561234561234561';
+		request.creditCard.emonth = '11';
+		request.creditCard.eyear = '2020';
+		request.creditCard.cvc = '123';
+		request.shopOrderid = 'InitiatePayment_'+(new Date()).getTime();
+		request.saleReconciliationIdentifier = 'my identifier yo';
+		request.authType = 'paymentAndCapture';
+
+		var response = papi.initiatePayment(request);
+
+		Assert.equals(true, response.success());
+		Assert.equals('my identifier yo', response.getReconciliationIdentifier());
 	}
 
 
