38339fcb418f39f9254d458ad86448261bacd645
emerson <et@altapay.com>
2017-02-06 17:14:36 +0100

    [p88]: Creation of an unit test

--- tests/unit/MerchantApiCallbackResponseTests.js
+++ tests/unit/MerchantApiCallbackResponseTests.js
@@ -19,5 +19,13 @@ var MerchantApiCallbackResponseTests = {
 		Assert.equals(321.12,response.getReservedAmount());
 		Assert.equals(0,response.getCapturedAmount());
 
+	},
+
+	ePaymentCancelled : function()
+	{
+		var response = api.parseCallbackXml('<?xml version="1.0"?><APIResponse version="20150526"><Header><Date>2015-09-07T09:31:44+02:00</Date><Path>API/ePaymentVerify?result=CANCEL&amp;payment_id=ae5e0de7-5238-4746-be1c-f3ef2b141a3a&amp;checksum=6c4eb3b36c95438ae09b519f2f458fc9&amp;token=EC-0DT17833PH498183L</Path><ErrorCode>0</ErrorCode><ErrorMessage/></Header><Body><Result>Cancelled</Result><Transactions><Transaction><TransactionId>25</TransactionId><PaymentId>ae5e0de7-5238-4746-be1c-f3ef2b141a3a</PaymentId><AuthType>payment</AuthType><CardStatus>NoCreditCard</CardStatus><CreditCardToken/><CreditCardMaskedPan/><ThreeDSecureResult>Not_Applicable</ThreeDSecureResult><LiableForChargeback>Merchant</LiableForChargeback><CVVCheckResult>Not_Applicable</CVVCheckResult><BlacklistToken/><ShopOrderId>payment-request-906354c2-c6b8-4cd4-9d8e-d6381ff66b28</ShopOrderId><Shop>AltaPay Shop Integration</Shop><Terminal>AltaPay Paypal Test Terminal</Terminal><TransactionStatus>epayment_cancelled</TransactionStatus><MerchantCurrency>840</MerchantCurrency><MerchantCurrencyAlpha>USD</MerchantCurrencyAlpha><CardHolderCurrency>840</CardHolderCurrency><CardHolderCurrencyAlpha>USD</CardHolderCurrencyAlpha><ReservedAmount>0.00</ReservedAmount><CapturedAmount>0.00</CapturedAmount><RefundedAmount>0.00</RefundedAmount><CreditedAmount>0.00</CreditedAmount><RecurringDefaultAmount>0.00</RecurringDefaultAmount><SurchargeAmount>0.00</SurchargeAmount><CreatedDate>2015-09-07 09:31:24</CreatedDate><UpdatedDate>2015-09-07 09:31:44</UpdatedDate><PaymentNature>Wallet</PaymentNature><PaymentSchemeName>PayPal</PaymentSchemeName><PaymentNatureService name="PaypalAcquirer"><SupportsRefunds>true</SupportsRefunds><SupportsRelease>true</SupportsRelease><SupportsMultipleCaptures>true</SupportsMultipleCaptures><SupportsMultipleRefunds>true</SupportsMultipleRefunds></PaymentNatureService><ChargebackEvents/><PaymentInfos/><CustomerInfo><UserAgent>Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36</UserAgent><IpAddress>127.0.0.1</IpAddress><Email/><Username/><CustomerPhone/><OrganisationNumber/><CountryOfOrigin><Country/><Source>NotSet</Source></CountryOfOrigin><RegisteredAddress><Firstname/><Lastname/><Address/><City/><Region/><Country/><PostalCode/></RegisteredAddress></CustomerInfo><ReconciliationIdentifiers/></Transaction></Transactions></Body></APIResponse>');
+
+		Assert.equals(false, response.success());
+		
 	}
 };
