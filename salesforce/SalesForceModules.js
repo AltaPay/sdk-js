58c35860768a88c47e88734bad6bec2f7d26c793
emerson <et@altapay.com>
2017-06-20 15:52:42 +0200

    [p36]: Changes required for SalesForce javascript plugin compatibility.

---
+++ salesforce/SalesForceModules.js
@@ -0,0 +1,17 @@
+/**
+ * Created by emerson on 6/20/17.
+ *
+ * Exporting of objects, necessary for the SalesForce (aka Demandware) javascript plugin.
+ *
+ */
+
+if (module != undefined) {
+
+    module.exports = {
+        ObjectHelper: ObjectHelper,
+        RhinoAltaPayFactory: RhinoAltaPayFactory,
+        AltaPayFactory: AltaPayFactory,
+        RhinoHttp: RhinoHttp
+    }
+
+}
